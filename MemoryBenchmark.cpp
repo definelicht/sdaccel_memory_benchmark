597f30a5b63b7cc7f6f40d6561b7ba1886744111
Johannes de Fine Licht <johannes.definelicht@inf.ethz.ch>
2017-03-09 15:29:10 +0100

    Reduce BRAM usage

--- MemoryBenchmark.cpp
+++ MemoryBenchmark.cpp
@@ -44,52 +44,52 @@ void MemoryBenchmark(
     Data_t *out1
 #endif
     ) {
-  #pragma HLS INTERFACE m_axi port=in0 offset=slave bundle=gmem0 \
-    depth=kBurstLength \
-    latency=kBurstLength \
-    max_read_burst_length=kBurstLength \
-    num_read_outstanding=4 \
-    max_write_burst_length=kBurstLength \
-    num_write_outstanding=4
-  #pragma HLS INTERFACE m_axi port=out0 offset=slave bundle=gmem0 \
-    depth=kBurstLength \
-    latency=kBurstLength \
-    max_read_burst_length=kBurstLength \
-    num_read_outstanding=4 \
-    max_write_burst_length=kBurstLength \
-    num_write_outstanding=4
+  #pragma HLS INTERFACE m_axi port=in0 offset=slave bundle=gmem0
+    // depth=kBurstLength \
+    // latency=kBurstLength \
+    // max_read_burst_length=kBurstLength \
+    // num_read_outstanding=4 \
+    // max_write_burst_length=kBurstLength \
+    // num_write_outstanding=4
+  #pragma HLS INTERFACE m_axi port=out0 offset=slave bundle=gmem0
+    // depth=kBurstLength \
+    // latency=kBurstLength \
+    // max_read_burst_length=kBurstLength \
+    // num_read_outstanding=4 \
+    // max_write_burst_length=kBurstLength \
+    // num_write_outstanding=4
   #pragma HLS INTERFACE s_axilite port=in0    bundle=control 
   #pragma HLS INTERFACE s_axilite port=out0   bundle=control 
   #pragma HLS INTERFACE s_axilite port=return bundle=control 
   #pragma HLS DATAFLOW
 #ifdef SDACCEL_MEMORYBENCHMARK_TUL_KU115
-  #pragma HLS INTERFACE m_axi port=in1 offset=slave bundle=gmem1 \
-    depth=kBurstLength \
-    latency=kBurstLength \
-    max_read_burst_length=kBurstLength \
-    num_read_outstanding=4 \
-    max_write_burst_length=kBurstLength \
-    num_write_outstanding=4
-  #pragma HLS INTERFACE m_axi port=out1 offset=slave bundle=gmem1 \
-    depth=kBurstLength \
-    latency=kBurstLength \
-    max_read_burst_length=kBurstLength \
-    num_read_outstanding=4 \
-    max_write_burst_length=kBurstLength \
-    num_write_outstanding=4
+  #pragma HLS INTERFACE m_axi port=in1 offset=slave bundle=gmem1
+    // depth=kBurstLength \
+    // latency=kBurstLength \
+    // max_read_burst_length=kBurstLength \
+    // num_read_outstanding=4 \
+    // max_write_burst_length=kBurstLength \
+    // num_write_outstanding=4
+  #pragma HLS INTERFACE m_axi port=out1 offset=slave bundle=gmem1
+    // depth=kBurstLength \
+    // latency=kBurstLength \
+    // max_read_burst_length=kBurstLength \
+    // num_read_outstanding=4 \
+    // max_write_burst_length=kBurstLength \
+    // num_write_outstanding=4
   #pragma HLS INTERFACE s_axilite port=in1  bundle=control 
   #pragma HLS INTERFACE s_axilite port=out1 bundle=control 
   hls::stream<Data_t> buffer0;
-  #pragma HLS STREAM variable=buffer0 depth=kBurstLength
+  #pragma HLS STREAM variable=buffer0 depth=256
   hls::stream<Data_t> buffer1;
-  #pragma HLS STREAM variable=buffer1 depth=kBurstLength
+  #pragma HLS STREAM variable=buffer1 depth=256
   Read(in0, buffer0);
   Read(in1, buffer1);
   Write(buffer0, out0);
   Write(buffer1, out1);
 #else
   hls::stream<Data_t> buffer;
-  #pragma HLS STREAM variable=buffer depth=kBurstLength
+  #pragma HLS STREAM variable=buffer depth=256
   Read(in0, buffer);
   Write(buffer, out0);
 #endif
